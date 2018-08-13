ab7eba9211dbdf9cefb4fafc9c0aa0c3136dd3e7
abhimanyu081 <abhimanyuk9871@gmail.com>
2018-08-13 20:10:30 +0530

    Added all Request Types Example in RestControllers

--- src/main/java/com/springboot/starter/topics/TopicController.java
+++ src/main/java/com/springboot/starter/topics/TopicController.java
@@ -3,7 +3,10 @@ package com.springboot.starter.topics;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,11 +24,30 @@ public class TopicController {
 				
 	}
 	
-	@RequestMapping("/topics")  //By Default its RequestMethod is GET
-	public Topic getTopic() {
+	@RequestMapping("/topics/{id}")  //By Default its RequestMethod is GET
+	public Topic getTopic(@PathVariable String id) {
 		
-		return topicService.getAllTopics();
+		return topicService.getTopic(id);
 				
+				
+	}
+	
+	@RequestMapping(value="/topics", method = RequestMethod.POST)  //By Default its RequestMethod is GET
+	public void addTopics(@RequestBody Topic topic) {
+		topicService.addTopic(topic);		
+				
+	}
+	
+	@RequestMapping(value="/topics/{id}", method = RequestMethod.PUT)  //By Default its RequestMethod is GET
+	public void updateTopic(@RequestBody Topic topic,
+			@PathVariable String id) {
+		topicService.addTopic(topic);		
+				
+	}
+	
+	@RequestMapping(value="/topics/{id}", method = RequestMethod.DELETE)  //By Default its RequestMethod is GET
+	public void deleteTopic(@PathVariable String id) {
+		topicService.delete(id);		
 				
 	}
 
--- src/main/java/com/springboot/starter/topics/TopicService.java
+++ src/main/java/com/springboot/starter/topics/TopicService.java
@@ -1,5 +1,6 @@
 package com.springboot.starter.topics;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,12 +9,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicService {
 	
-	private List<Topic> topics = Arrays.asList(
+	private List<Topic> topics = new ArrayList<>(Arrays.asList(
 
 			new Topic("1", "spring", "spring description"), new Topic("2", "hibernate", "hibernate description"),
 			new Topic("3", "redis", "redis description"), new Topic("4", "mongo", "mongo description")
 
-	);
+	));
 
 	public List<Topic> getAllTopics() {
 
@@ -25,6 +26,25 @@ public class TopicService {
 		
 		return topics.stream().filter(t->t.getId().equals(id)).findFirst().get();
 		
+	}
+	
+	public void addTopic(Topic topic ) {
+		topics.add(topic);
+	}
+	
+	public void updateTopic(String id, Topic topic ) {
+		
+		for(int i=0;i<topics.size();i++) {
+			if(topics.get(i).getId().equals(id)) {
+				topics.set(i, topic);
+				break;
+			}
+		}
+		
+	}
+
+	public void delete(String id) {
+		topics.removeIf(t->t.getId().equals(id));
 	}
 
 }
